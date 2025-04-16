490df24183b62a25e0bd6fc3929a1e5e6e7cea5d
Antoine <antoine@enimaloc.fr>
2025-04-16 17:02:30 +0200

    feat(settings): add settings

--- ONITwitchBridge/ONITwitchBridge.cs
+++ ONITwitchBridge/ONITwitchBridge.cs
@@ -1,6 +1,17 @@
-﻿namespace ONITwitchBridge
+﻿using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Core;
+using PeterHan.PLib.Options;
+
+namespace ONITwitchBridge
 {
-    public class ONITwitchBridge
+    public class ONITwitchBridge : UserMod2
     {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary(false);
+            new POptions().RegisterOptions(this, typeof(Settings));
+        }
     }
 }
---
+++ ONITwitchBridge/Settings.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using PeterHan.PLib.Options;
+
+namespace ONITwitchBridge
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Settings
+    {
+        [Option("Username", "The username of the bot. You can set to justinfan12345 for anonymous",
+            "Twitch")]
+        [JsonProperty]
+        public string Username { get; set; } = "justinfan12345";
+
+        private string _OAuth = "oauth:kappa";
+
+        [Option("OAuth",
+            "The OAuth token of the bot. If username is justinfan12345 you can fill with a placeholder value",
+            "Twitch")]
+        [JsonProperty]
+        public string OAuth
+        {
+            get => _OAuth;
+            set
+            {
+                if (value.StartsWith("oauth:"))
+                {
+                    _OAuth = value;
+                }
+                else
+                {
+                    _OAuth = "oauth:" + value;
+                }
+            }
+        }
+        
+        [Option("Channel", "The channel to join.", "Twitch")]
+        [JsonProperty]
+        public string Channel { get; set; } = "enimaloc";
+
+        [Option("Save type", "Select how registered users are saved.")]
+        [JsonProperty]
+        public SaveType SaveType { get; set; } = SaveType.Colony;
+    }
+
+    public enum SaveType
+    {
+        Colony,
+        Global,
+        Session
+    }
+}
